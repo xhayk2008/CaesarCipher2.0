8c462d8a465c0326224ce4369476f2f4589af920
Hayk Khachatryan <my_universal_account@protonmail.com>
2024-12-11 16:48:42 +0400

    Added possibility to separate encode and decode text

--- CaesarCipher2.0/Program.cs
+++ CaesarCipher2.0/Program.cs
@@ -4,19 +4,28 @@
     {
         static void Main(string[] args)
         {
+
             Console.Write("Text: ");
             string text = Console.ReadLine() ?? String.Empty;
+
             Console.Write("Key: ");
             bool converting = int.TryParse(Console.ReadLine(), out int key);
 
-            string EncodedCode = Encode.encode(text, key);
+            ConsoleKey consolekey = Console.ReadKey().Key;
 
-            string DecodedCode = Decode.decode(EncodedCode, key);
-
-            Console.WriteLine();
-
-            Console.WriteLine($"Encoded Text: {EncodedCode}");
-            Console.WriteLine($"Decoded Text: {DecodedCode}");
+            switch (consolekey)
+            {
+                case ConsoleKey.E:
+                    Console.Clear();
+                    string EncodedCode = Encode.encode(text, key);
+                    Console.WriteLine($"Encoded Text: {EncodedCode}");
+                    break;
+                case ConsoleKey.D:
+                    Console.Clear();
+                    string DecodedCode = Decode.decode(text, key);
+                    Console.WriteLine($"Decoded Text: {DecodedCode}");
+                    break;
+            }
         }
     }
 }
